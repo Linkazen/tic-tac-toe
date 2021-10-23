1efc58416518b680d2531cc60988b8320b04f0e1
Jonathan <Jpch2k4@gmail.com>
2021-10-23 16:50:24 +0100

    Added board generation in the javascript section.

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+gameArea = document.querySelector("#gameboard")
+
+
 const gameboard = (() => {
-    let squares = 3 * 3
+    const squares = function() {
+        let array = []
+        for(i = 0; i < 9; i++) {
+            array.push(`${i}`)
+        }
+        console.log(array)
+        return array
+    }
     return {squares}   
-})
+})()
 
 const player = (playName) => {
     let score = 0
     return {playName, score}
 }
+
+function generateBoard() {
+    let grid = gameboard.squares()
+    for(i = 0; i < grid.length; i++) {
+        let gridNum = document.createElement("div")
+        gridNum.setAttribute(`id`, `grid${i}`)
+        gridNum.textContent = "X"
+        gameArea.appendChild(gridNum)
+    }
+}
+
+generateBoard()
