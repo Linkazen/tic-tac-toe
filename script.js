9df8035f29e41e86699aa34d175f83f6c4dc01a5
Jonathan <Jpch2k4@gmail.com>
2021-10-24 11:37:51 +0100

    Added click listeners to the grid to add a class to any of the divs when clicked

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-gameArea = document.querySelector("#gameboard")
+gameArea = document.querySelector(`#gameboard`)
 
 
 const gameboard = (() => {
-    const squares = function() {
-        let array = []
-        for(i = 0; i < 9; i++) {
-            array.push(`${i}`)
-        }
-        console.log(array)
-        return array
-    }
+    const squares = new Array(9)
     return {squares}   
 })()
 
@@ -18,14 +11,26 @@ const player = (playName) => {
     return {playName, score}
 }
 
+function makeXOrO(e) {
+    e.srcElement.classList.add(`X`)
+    console.log(e.srcElement.classList)
+}
+
 function generateBoard() {
-    let grid = gameboard.squares()
+    let grid = gameboard.squares
     for(i = 0; i < grid.length; i++) {
         let gridNum = document.createElement("div")
         gridNum.setAttribute(`id`, `grid${i}`)
-        gridNum.textContent = "X"
+        gridNum.setAttribute(`class`, `box`)
+        gridNum.textContent = `X`
         gameArea.appendChild(gridNum)
     }
+    let boxes = document.querySelectorAll(`.box`)
+    for(i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('click', makeXOrO)
+    }
 }
+
+
 
 generateBoard()
