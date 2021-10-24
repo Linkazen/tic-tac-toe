0ebd13638f4589ec85c2030c4bfcab75210795fe
Jonathan <Jpch2k4@gmail.com>
2021-10-24 11:46:36 +0100

    Cleaned the functions into the modules

--- script.js
+++ script.js
@@ -3,7 +3,31 @@ gameArea = document.querySelector(`#gameboard`)
 
 const gameboard = (() => {
     const squares = new Array(9)
-    return {squares}   
+
+    function makeXOrO(e) {
+        e.srcElement.classList.add(`X`)
+        console.log(e.srcElement.classList)
+    }
+
+    function boardGen() {
+        for(i = 0; i < squares.length; i++) {
+            let gridNum = document.createElement("div")
+            gridNum.setAttribute(`id`, `grid${i}`)
+            gridNum.setAttribute(`class`, `box`)
+            gridNum.textContent = `X`
+            gameArea.appendChild(gridNum)
+        }
+        let boxes = document.querySelectorAll(`.box`)
+        for(i = 0; i < boxes.length; i++) {
+            boxes[i].addEventListener('click', makeXOrO)
+        }
+    }
+
+    return {
+        generateBoard: function() {
+            boardGen();
+        }
+    }   
 })()
 
 const player = (playName) => {
@@ -11,26 +35,5 @@ const player = (playName) => {
     return {playName, score}
 }
 
-function makeXOrO(e) {
-    e.srcElement.classList.add(`X`)
-    console.log(e.srcElement.classList)
-}
 
-function generateBoard() {
-    let grid = gameboard.squares
-    for(i = 0; i < grid.length; i++) {
-        let gridNum = document.createElement("div")
-        gridNum.setAttribute(`id`, `grid${i}`)
-        gridNum.setAttribute(`class`, `box`)
-        gridNum.textContent = `X`
-        gameArea.appendChild(gridNum)
-    }
-    let boxes = document.querySelectorAll(`.box`)
-    for(i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('click', makeXOrO)
-    }
-}
-
-
-
-generateBoard()
+gameboard.generateBoard()
